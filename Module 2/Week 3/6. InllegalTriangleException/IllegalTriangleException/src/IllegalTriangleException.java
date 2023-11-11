9fbe326d5cbb2423b634def1050b9b338d81104b
LamChiHung <146713547+LamChiHung@users.noreply.github.com>
2023-11-11 19:20:31 +0700

    create illegal triangle exception

---
+++ Module 2/Week 3/6. InllegalTriangleException/IllegalTriangleException/src/IllegalTriangleException.java
@@ -0,0 +1,5 @@
+public class IllegalTriangleException extends Exception {
+    public IllegalTriangleException() {
+        System.out.println("The length is not valid");
+    }
+}
---
+++ Module 2/Week 3/6. InllegalTriangleException/IllegalTriangleException/src/Main.java
@@ -0,0 +1,37 @@
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class Main {
+    public static boolean checkTriangle(int a, int b, int c) {
+        int[] arr = {a, b, c};
+        Arrays.sort(arr);
+
+        if (a <= 0 | b <= 0 | c <= 0) {
+            return false;
+        } else if (arr[2] >= arr[0] + arr[1]) {
+            return false;
+        } else {
+            return true;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        try {
+            System.out.println("Input first side: ");
+            int a = sc.nextInt();
+            System.out.println("Input second side: ");
+            int b = sc.nextInt();
+            System.out.println("Input third side: ");
+            int c = sc.nextInt();
+            if (checkTriangle(a, b, c)) {
+                System.out.println("This is triangle");
+            } else {
+                throw new IllegalTriangleException();
+            }
+        } catch (IllegalTriangleException e) {
+            System.out.println(e);
+        }
+    }
+}
