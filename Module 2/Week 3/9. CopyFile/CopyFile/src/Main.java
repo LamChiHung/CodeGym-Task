bce4b999f37d3e7e09936450dd70ac01c0713bda
LamChiHung <146713547+LamChiHung@users.noreply.github.com>
2023-11-12 21:19:06 +0700

    create copy file

---
+++ Module 2/Week 3/9. CopyFile/CopyFile/src/Main.java
@@ -0,0 +1,38 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.out.println("Hello world!");
+        File fileSource = new File(".\\fileSource.txt");
+        File fileTarget = new File(".\\fileTarget.txt");
+        if (! fileSource.exists()) {
+            System.out.println("File chưa tồn tại, khởi tạo file mới");
+            try (PrintWriter pw = new PrintWriter(fileSource)) {
+                pw.println("ABCD");
+                pw.println("EFGH");
+                pw.println(123);
+                pw.flush();
+            } catch (IOException e) {
+                System.out.println(e.getMessage());
+            }
+        }
+
+        try (PrintWriter pw = new PrintWriter(fileTarget)) {
+            try (FileReader fr = new FileReader(fileSource)) {
+                BufferedReader br = new BufferedReader(fr);
+                String string = "";
+                while ((string = br.readLine()) != null) {
+                    pw.println(string);
+                }
+            }
+        } catch (IOException e) {
+            System.out.println(e.getMessage());
+        }
+    }
+
+
+}
