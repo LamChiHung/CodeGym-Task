9e94746429ec2f025489f1dc8e09127e0cae9da2
LamChiHung <146713547+LamChiHung@users.noreply.github.com>
2023-11-14 22:10:34 +0700

    update validate class name

--- Module 2/Week 3/23. Validate ClassName/ValidateClassName/src/Main.java
+++ Module 2/Week 3/23. Validate ClassName/ValidateClassName/src/Main.java
@@ -1,8 +1,11 @@
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Main {
     public static void main(String[] args) {
+        String classname1 = "C0223G";
         Pattern pattern = Pattern.compile("^[CAP]{1}\\d{4}[GHIK]$");
-        
+        Matcher matcher = pattern.matcher(classname1);
+        System.out.println(matcher.matches());
     }
 }
