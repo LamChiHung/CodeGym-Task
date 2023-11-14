4927791e299a5684cb17c94dfb396cf642e6958c
LamChiHung <146713547+LamChiHung@users.noreply.github.com>
2023-11-14 22:41:25 +0700

    create valid phoneNumber

---
+++ Module 2/Week 3/24. Validate PhoneNumber/ValidatePhoneNumber/src/Main.java
@@ -0,0 +1,11 @@
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class Main {
+    public static void main(String[] args) {
+        String phoneNumber = "84-0978489648";
+        Pattern pattern = Pattern.compile("(^[0-9]{2})-(0[0-9]{9})");
+        Matcher matcher = pattern.matcher(phoneNumber);
+        System.out.println(matcher.matches());
+    }
+}
