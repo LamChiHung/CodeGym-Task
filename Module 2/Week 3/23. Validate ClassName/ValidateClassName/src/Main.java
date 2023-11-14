b495f444bdcce45dd0fc403e4d927c1289e466e2
LamChiHung <146713547+LamChiHung@users.noreply.github.com>
2023-11-14 22:04:21 +0700

    create validate class name

---
+++ Module 2/Week 3/23. Validate ClassName/ValidateClassName/src/Main.java
@@ -0,0 +1,8 @@
+import java.util.regex.Pattern;
+
+public class Main {
+    public static void main(String[] args) {
+        Pattern pattern = Pattern.compile("^[CAP]{1}\\d{4}[GHIK]$");
+        
+    }
+}
