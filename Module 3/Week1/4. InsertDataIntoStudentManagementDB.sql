f939034c4365c0a971992a65430043e9dd469219
LamChiHung <146713547+LamChiHung@users.noreply.github.com>
2023-12-07 10:40:07 +0700

    insert data to studentmanagement sql

---
+++ Module 3/Week1/4. InsertDataIntoStudentManagementDB.sql
@@ -0,0 +1,32 @@
+-- USE studentmanagement;
+
+-- ALTER TABLE class MODIFY COLUMN startDate DATE ; 
+
+-- INSERT INTO Class(className, startDate, status)
+-- values('A1','2008-12-20',1);
+
+-- INSERT INTO Class(className, startDate, status)
+-- values('A2','2008-12-22',1);
+
+-- INSERT INTO Class(className, startDate, status)
+-- values('B3',curdate(),1);
+
+-- INSERT INTO student(studentName, address, phone, status, classID)
+-- 	VALUES('Hung','Ha Noi', '0912113113', 1, 1);
+
+-- INSERT INTO student(studentName, address, status, classID)
+-- 	VALUES('Hoa','Hai Phong', 1, 1);
+  
+-- INSERT INTO student(studentName, address, phone, status, classID)
+-- 	VALUES('Manh','HCM', '0123123123', 0, 2);
+    
+    
+--  INSERT INTO subject(subname, credit, status) VALUES ('CF',5,1);
+-- 	INSERT INTO subject(subname, credit, status) VALUES ('C',6,1);
+-- 	INSERT INTO subject(subname, credit, status) VALUES ('HDJ',5,1);
+-- 	INSERT INTO subject(subname, credit, status) VALUES ('RDBMS',10,1);
+
+
+-- INSERT INTO mark (subjectID,studentID,mark,examTimes) VALUES (1,1,8,1);
+-- INSERT INTO mark (subjectID,studentID,mark,examTimes) VALUES (1,2,10,2);
+-- INSERT INTO mark (subjectID,studentID,mark,examTimes) VALUES (2,1,12,1);
